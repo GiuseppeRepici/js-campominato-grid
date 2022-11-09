8a0ca6f70884852e63581b5c2d6fd15058d1ea8f
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-09 15:42:13 +0100

    completed senza bonus

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 const btn = document.getElementById("btn");
-const gridContainer = document.getElementById("grid-cont");
+let gridContainer = document.getElementById("grid-cont");
 
 
 btn.addEventListener("click", function() {
+   
     for(let i=0; i<100; i++){
         const newCell = document.createElement("div");
         newCell.classList.add("cella");
         newCell.style.width = "calc(100% / 10 )";
 
-        newCell.innerHTML= i;
+        newCell.innerHTML = i+1;
         gridContainer.append(newCell);
+        newCell.addEventListener("click", function() {
+            const numeroDentro = parseInt( this.textContent );
+            newCell.classList.toggle("azzurro");
+            console.log(numeroDentro)
+        })
     }
 
 })
