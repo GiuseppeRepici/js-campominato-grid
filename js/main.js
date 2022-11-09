9b0b21d5ff0a31026d2f8f1e5fae4175640907fa
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-09 16:10:29 +0100

    quasi completed

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ let gridContainer = document.getElementById("grid-cont");
 
 
 btn.addEventListener("click", function() {
-   
-    for(let i=0; i<100; i++){
+    let difficolta = document.getElementById("selectdiff");
+    console.log(difficolta.value);
+    let numCelleRiga = Math.sqrt(`${difficolta.value}`);
+    console.log(numCelleRiga);
+
+    
+    for(let i=0; i<difficolta.value; i++){
         const newCell = document.createElement("div");
         newCell.classList.add("cella");
-        newCell.style.width = "calc(100% / 10 )";
+        newCell.style.width = `calc(100% / ${numCelleRiga} )`;
 
         newCell.innerHTML = i+1;
         gridContainer.append(newCell);
